5803abd4c0d8d8bb62df0a169516fd8b6f7846c2
Guilherme isabelle <isabelle.guilherme@csenen.com>
2020-12-02 06:30:16 +0100

    rajout de commentaires et modif css

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,4 +1,13 @@
 
+//changement de l'image de l'avatar lorsque l'on clic dessus dans la section desktop
 document.querySelector(".desktop").addEventListener("click", function(event){
     document.querySelector(".imageAvatar").src = "image/avatar-bis.png";
 })
+
+
+//changement du prénom et de la couleur d'écriture avec un prompt dans la section presentation
+//document.querySelector(".changeName").addEventListener("click", function(event)){
+//    document.querySelector("#name")
+//}
+
+
